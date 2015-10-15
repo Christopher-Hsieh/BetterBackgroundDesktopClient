fe3e82a313c2396916670899785d89f3df694594
Hsieh11 <Chrishsieh@live.com>
2015-10-15 18:52:22 -0400

    Added listeners to the Login GUI + simple test

---
+++ test/com/betterbackground/gui/test/TestGUI.java
@@ -0,0 +1,26 @@
+package com.betterbackground.gui.test;
+
+import static org.junit.Assert.*;
+
+import java.net.URISyntaxException;
+
+import org.junit.Test;
+
+import com.betterbackground.gui.Login;
+
+public class TestGUI {
+
+	@Test
+	public void test() throws URISyntaxException, InterruptedException {
+		fail("Not yet implemented");
+		Login login = new Login();
+		
+		login.setupLoginUI();
+		login.createLoginUI();
+		login.createLoginListener();
+		login.buttonLogin.doClick();
+		
+		
+	}
+
+}
